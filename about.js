3b786caa14ce8ebdb5453e1a743b4b7ffd2326b0
Sun-G0D <evan.sun.ca@gmail.com>
2025-04-09 12:49:13 -0700

    juice
    copy to clipoboard

--- about.js
+++ about.js
@@ -2,14 +2,10 @@ function copy_to_clipboard() {
     // Get the text field
     var copyText = 'evan.sun.ca@gmail.com';
   
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
      // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
+    navigator.clipboard.writeText(copyText);
   
     // Alert the copied text
-    alert("email copied" + copyText.value);
+    alert("email copied");
   }
 
